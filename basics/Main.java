7eab85189868e2aa9751ff0042cf213feb51a497
Cristian Restrepo <crestrepo21@gmail.com>
2019-05-28 12:49:57 -0700

    lab completed

--- basics/Main.java
+++ basics/Main.java
@@ -1,19 +1,54 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
   public static void main(String[] args) {
-    int dogCount = 1;
-    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
-
-    int catCount = 2;
-    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
-
-    int turtleCount = 0;
-    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
-
+    // pluralize("dog", 0);
+    // pluralize("cat", 1);
+    // pluralize("turtle", 2);
+    // flipNHeads(4);
+    clock();
+    
   }
-  public static String pluralize(String word, int count) {
+  
+  public static void pluralize(String word, int count) {
+    
     if (count > 1 || count == 0){
-      return word + "s";
+      System.out.println("I own " + count + " " + word + "s") ;
+    }else {
+      System.out.println("I own " + count + " " + word) ;
     }
-    return word;
+  }
+  
+  public static void flipNHeads(int n){
+    int flips = 0;
+    int heads = 0;
+    do{
+      double i = Math.random();
+      
+      if (i < .5){
+        System.out.println("tails");
+        flips ++;
+        heads = 0;
+      } else {
+        System.out.println("heads");
+        flips ++;
+        heads++;
+      }
+      
+    } while (heads < n);
+    System.out.println("It took " + flips + " flips to flip " + n + " heads in a row.");
+    
+  }
+
+  public static void clock(){
+    LocalDateTime now = LocalDateTime.now();
+
+    while(true){
+      if (now.getSecond() != LocalDateTime.now().getSecond()){
+        System.out.println(now.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        now = LocalDateTime.now();
+      }
+    }
   }
 }
