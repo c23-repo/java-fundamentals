41261de9ea8d1892b2f1d50b94066ec8982d4639
Cristian Restrepo <crestrepo21@gmail.com>
2019-05-28 11:17:30 -0700

    first task working

---
+++ basics/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    int dogCount = 1;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+    int catCount = 2;
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+    int turtleCount = 0;
+    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+  }
+  public static String pluralize(String word, int count) {
+    if (count > 1 || count == 0){
+      return word + "s";
+    }
+    return word;
+  }
+}
