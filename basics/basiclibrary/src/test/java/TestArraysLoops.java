0b75705a8b3d1c8c4e7c932f292b73c4a164b4f9
Cristian Restrepo <crestrepo21@gmail.com>
2019-05-30 22:07:30 -0700

    lab 2 work on lab 3 branch

--- basics/basiclibrary/src/test/java/TestArraysLoops.java
+++ basics/basiclibrary/src/test/java/TestArraysLoops.java
@@ -24,4 +24,26 @@ public class TestArraysLoops {
         assertFalse("Array should return false if no duplicates", classForTesting.containsDuplicates(inputArr));
     }
 
+    @Test public void testCalculatingAverages(){
+        ArraysLoops classForTesting = new ArraysLoops();
+        int []inputArr = new int[]{1, 2, 3, 4, 5};
+        double expectedOutput = 3.0;
+
+        assertEquals("This should return the average of the array.", expectedOutput,
+                classForTesting.calculatingAverages(inputArr), 0);
+    }
+
+    @Test public void testAverageOfAverage(){
+        ArraysLoops classForTesting = new ArraysLoops();
+
+        int[][] arr = new int[][]{
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {4, 8, 12, 16, 20}
+        };
+        double expectedOutput = 3.0;
+        assertEquals("this should print out an avg of 3 for each index in the array",
+                expectedOutput, classForTesting.averageOfAverages(arr), 0);
+    }
+
 }
